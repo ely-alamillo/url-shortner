0ca11cd4d788819e2091488135aaf5e0e1af2595
Ely Alamillo <ely0013@gmail.com>
2017-11-17 13:18:24 -0600

    added pre-save hook to handle restarts

--- api/models/UrlModel.js
+++ api/models/UrlModel.js
@@ -9,22 +9,27 @@ const counterSchema = Schema({
 const counter = mongoose.model('counter', counterSchema);
 
 const urlSchema = new Schema({
-  _id: { type: String, index: true },
+  _id: { type: String },
   long_url: { type: String },
   created_at: { type: Date }
 });
 
+// hook checks id url_count is initaliazed if not it intializes it
+urlSchema.pre('save', function(next) {
+  counter.findOne({ _id: 'url_count' }, function(err, counter) {
+    if (err || !counter) initCounter();
+  });
+  next();
+});
+
+// increments the counter for each new entry
 urlSchema.pre('save', function(next) {
   const doc = this;
-  // if (startCount) {
-  //   initCounter();
-  //   startCount = false;
-  // }
   // increments the current counter +1 and sets it to be _id of new entry
   counter.findByIdAndUpdate({ _id: 'url_count' }, { $inc: { seq: 1 } }, { new: true }, function(err, counter) {
     if (err) return next(err);
     doc.created_at = new Date();
-    doc._id = counter.seq;
+    doc._id = counter.seq - 1;
     next();
   });
 });
@@ -34,7 +39,6 @@ const initCounter = () => {
   startCount.save(err => console.log(err));
 };
 
-initCounter();
 
 const Url = mongoose.model('Url', urlSchema);
 
