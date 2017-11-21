3da54d034d8cf6f4764ad4441b27ec9eea8493f3
Ely Alamillo <ely0013@gmail.com>
2017-11-21 16:23:56 -0600

    working on styling

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './About.css';
 
 const About = () => {
   return (
-    <div className='jumbotron'>
-      <div className='container'>
-        <p className='lead'>
-          URL Shortener is a project built to shorten URLs. It is
-          implemented using a Base58 encoding algorithm to create
-          unique identifiers for each URL. The identifiers are used to create
-          a shorter URL that will redirect to the original destination.
-        </p>
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="#" role="button">Source on GitHub</a>
-        </p>
+    <div className='container about'>
+      <div className='jumbotron'>
+        <div className='container'>
+          <p className='lead text-left'>
+            URL Shortener is a project built to shorten URLs. It is
+            implemented using a Base58 encoding algorithm to create
+            unique identifiers for each URL. The identifiers are used to create
+            a shorter URL that will redirect to the original destination.
+          </p>
+          <p class="lead text-left">
+            <a class="btn btn-primary btn-lg" href="https://github.com/ely-alamillo/urlshortner" role="button">Source on GitHub</a>
+            <Link to='/' className='btn btn-lg btn-default'> Try Me</Link>
+          </p>
+        </div>
       </div>
     </div>
   );
